f5d5bf8584d7eca351737d30bb008d54ee92b03b
TPMPep <paulette.pantoja@transperfect.com>
2026-08-22 23:13:19 -0700

    Update build-tag.ts

--- src/build-tag.ts
+++ src/build-tag.ts
@@ -29,4 +29,4 @@
 // spending a refinement run.
 // =============================================================================
 
-export const BUILD_TAG = '2026-08-22b-me-uuid-v4-delayed-liveness';
+export const BUILD_TAG = '2026-08-22c-me-poll-persistent-singleton';
