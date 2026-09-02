e8454b61fa7ebdd7183fd4f095d9cea55ad468ba
TPMPep <paulette.pantoja@transperfect.com>
2026-09-02 09:33:02 -0700

    Update build-tag.ts

--- src/build-tag.ts
+++ src/build-tag.ts
@@ -29,4 +29,4 @@
 // spending a refinement run.
 // =============================================================================
 
-export const BUILD_TAG = '2026-09-02d-approved-script-acoustic-segmentation';
+export const BUILD_TAG = '2026-09-02e-pre-export-audio-qc';
